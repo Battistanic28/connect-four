7ee1f7871d798879940b2bb5044f7b4aa1b410d6
Nick Battista <nickbattista@ashley.day>
2020-11-03 16:29:54 -0700

    added board html, div styling, and evernt listeners

--- connect4.js
+++ connect4.js
@@ -8,23 +8,33 @@
 const WIDTH = 7;
 const HEIGHT = 6;
 
-const currPlayer = 1; // active player: 1 or 2
+let currPlayer = 1; // active player: 1 or 2
 const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
+// **** STEP 3 ****
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  const board  = [
+    [ null, null, null, null, null, null, null ],
+    [ null, null, null, null, null, null, null ],
+    [ null, null, null, null, null, null, null ],
+    [ null, null, null, null, null, null, null ],
+    [ null, null, null, null, null, null, null ],
+    [ null, null, null, null, null, null, null ],
+  ];
 }
 
+// **** STEP 4 ****
 /** makeHtmlBoard: make HTML table and row of column tops. */
-
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
-
+  const htmlBoard = document.getElementById('board');
   // TODO: add comment for this code
+  // Dynamically generate top row of game board with click event listener
   const top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
@@ -36,7 +46,9 @@ function makeHtmlBoard() {
   }
   htmlBoard.append(top);
 
-  // TODO: add comment for this code
+  // Dynamically generate table rows consisting of data cells
+  // Table rows = HEIGHT
+  // Table cells = WIDTH 
   for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
     for (var x = 0; x < WIDTH; x++) {
@@ -56,9 +68,14 @@ function findSpotForCol(x) {
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
-
+// **** STEP 5 ****
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
+const newDiv = document.createElement('div');
+newDiv.setAttribute('class', 'piece')
+newDiv.classList.add(`P${currPlayer}`)
+const place = document.getElementById(`${y}-${x}`)
+place.appendChild(newDiv);
 }
 
 /** endGame: announce game end */
@@ -68,31 +85,41 @@ function endGame(msg) {
 }
 
 /** handleClick: handle click of column top to play piece */
-
+// **** STEP 6 ****
 function handleClick(evt) {
   // get x from ID of clicked cell
   const x = +evt.target.id;
-
   // get next spot in column (if none, ignore click)
   const y = findSpotForCol(x);
   if (y === null) {
     return;
   }
-
+  
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
   placeInTable(y, x);
-
+  
   // check for win
   if (checkForWin()) {
     return endGame(`Player ${currPlayer} won!`);
   }
-
+  
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
-
+  
+  
   // switch players
   // TODO: switch currPlayer 1 <-> 2
+  togglePlayers(currPlayer)
+  
+}
+
+function togglePlayers(player) {
+  if (player === 1) {
+    return player + 1
+  } else {
+    return player = 1
+  }
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -131,3 +158,4 @@ function checkForWin() {
 
 makeBoard();
 makeHtmlBoard();
+
