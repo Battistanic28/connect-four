1782d87a692943dca3593b976f9a3bf93846ef51
Nick Battista <nickbattista@ashley.day>
2020-11-05 18:17:13 -0700

    fixed check for winner test - Game fully functional

--- connect4.js
+++ connect4.js
@@ -9,7 +9,15 @@ const WIDTH = 7;
 const HEIGHT = 6;
 
 let currPlayer = 1; // active player: 1 or 2
-let board = []; // array of rows, each row is array of cells  (board[y][x])
+let board = [
+  [ null, null, null, null, null, null, null ],
+  [ null, null, null, null, null, null, null ],
+  [ null, null, null, null, null, null, null ],
+  [ null, null, null, null, null, null, null ],
+  [ null, null, null, null, null, null, null ],
+  [ null, null, null, null, null, null, null ],
+];
+ // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -17,15 +25,7 @@ let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
-   board = [
-    [ null, null, null, null, null, null, null ],
-    [ null, null, null, null, null, null, null ],
-    [ null, null, null, null, null, null, null ],
-    [ null, null, null, null, null, null, null ],
-    [ null, null, null, null, null, null, null ],
-    [ null, null, null, null, null, null, null ],
-  ];
-  return board
+
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
@@ -58,20 +58,18 @@ function makeHtmlBoard() {
     htmlBoard.append(row);
   }
 }
-// ******************************************************************************
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 function findSpotForCol(x) {
   for (let y = 5; y >= 0; y--) {
     if (!document.getElementById(`${y}-${x}`).hasChildNodes()) {
-        return y
+        return y;
     }
   }
-  return y;
+  return null;
+
 }
-// ******************************************************************************
 
 /** placeInTable: update DOM to place piece into HTML table of board */
-// ******************************************************************************
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
 const newDiv = document.createElement('div');
@@ -80,15 +78,12 @@ newDiv.classList.add(`P${currPlayer}`)
 const place = document.getElementById(`${y}-${x}`)
 place.appendChild(newDiv);
 }
-// ******************************************************************************
 
 /** endGame: announce game end */
-// ******************************************************************************
 function endGame(msg) {
   // TODO: creae end game message alert
-    return alert(msg)
+    return setTimeout(function() {alert(msg)}, 200);
 }
-// ******************************************************************************
 
 /** handleClick: handle click of column top to play piece */
 function handleClick(evt) {
@@ -103,15 +98,16 @@ function handleClick(evt) {
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
   placeInTable(y, x);
-
-  // switch players
-  // TODO: switch currPlayer 1 <-> 2
-  currPlayer = togglePlayers(currPlayer)
+  board[y][x] = currPlayer;
   
   // check for win
   if (checkForWin()) {
     return endGame(`Player ${currPlayer} won!`);
   }
+
+    // switch players
+  // TODO: switch currPlayer 1 <-> 2
+  currPlayer = togglePlayers(currPlayer)
   
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
